9ac5b0aa87f0582e7d64f72cbae3f3e7454c4ed4
Gabriel Lepetit-Aimon <gabriel.lepetitaimon@gmail.com>
2018-03-20 10:19:04 -0400

    Add first sketches

--- src/assets/scripts/1-preproc.js
+++ src/assets/scripts/1-preproc.js
@@ -2,17 +2,115 @@
  
 /**
  * 
- * @param roadIncident   Données non prétraitées issues de incident.csv.
- * @param roadIncident   Données non prétraitées issues de population.csv.
- * @return Dictionnaire de données par pays au format:
+ * @param countryInfos   Données non prétraitées issues de countryInfos.csv.
+ * @return Dictionnaire de descriptions des pays:
  *              {
- *                  'France':{
- *                      1950: []
- *                   },
+ *                  'France':{continent: 'Europe', code: 'FRA'},
  *                  'Egypt': ...
  *              }
  *                  
  */
-preproc = function(roadIncident, population){
-    // TODO
+var preproccessCountries = function(countryInfos){
+    var countries = {};
+    countryInfos.forEach(function(d){
+        countries[d['pays']] = {continent: d['Continent'], code: d['code']}
+    });
+    
+    return countries;
+}
+
+/**
+ * 
+ * @param roadIncident   Données non prétraitées issues de incident.csv.
+ * @param roadIncident   Données non prétraitées issues de population.csv.
+ * @return Dictionnaire de données par pays au format:
+ *              [
+ *                  {'pays': 'France',
+ *                   'data': [   
+ *                      {'annee': 1970, 'popu all': , 'popu female': , 'popu F 00-14': , 'rate all': , ...},
+ *                      {'annee': 1971: ...}]
+ *                   },
+ *                  {'pays': 'Egypt', 
+ *                      ...
+ *                  }
+ *              ]
+ *                  
+ */
+var preproccess = function(roadIncident, population, countries){
+    var dict = {};
+    for(var country in countries)
+        dict[country] = {};
+    
+    var items = ['all', 'F', 'M', 'F 00-14', 'M 00-14', 'F 15-29', 'M 15-29', 'F 30-44', 'M 30-44', 'F 45-59', 'M 45-59', 'F 60+', 'M 60+', '00-14', '15-29', '30-44', '45-59', '60+'];
+    
+    
+    roadIncident.forEach(function(incident){
+        var year = incident['annee'], country = incident['pays'];
+        dict[country][year] = {};
+        
+        // Store data
+        var d = dict[country][year];
+        items.forEach(function(item){d['rel '+item] = parseFloat(incident[item]);});
+        items.forEach(function(item){d['abs '+item] = NaN;});
+    });
+    
+    population.forEach(function(pop){
+        var year = pop['annee'], country = pop['pays'];
+        if(country in dict && year in dict[country]){
+            var d = dict[country][year];
+            items.forEach(function(item){d['abs '+item] = Math.round(d['rel '+item]*pop[item]/1000000);});
+        }
+    });
+    
+    var data = [];
+    for(var country in dict){
+        var d_country = [];
+        for(var year in dict[country]){
+            var d = {annee: year}
+            var country_year_data = dict[country][year];
+            for(var item in country_year_data)
+                d[item] = country_year_data[item];
+            d_country.push(d)
+        }
+        data.push({pays: country, data: d_country});
+    }
+    
+    return data;
+}
+
+
+/**
+ * 
+ * @param x   Scale à definir
+ * @param data    Données à afficher
+ *                  
+ */
+var domainX = function(x, data){
+    var min = d3.min(data, pays => d3.min(pays.data, d=>d.annee));
+    var max = d3.max(data, pays => d3.max(pays.data, d=>d.annee));
+    x.domain([min, max]);
+}
+
+/**
+ * 
+ * @param y       Scale à definir
+ * @param data    Données à afficher
+ *                  
+ */
+var domainY = function(y, data){
+    var min = d3.min(data, pays => d3.min(pays.data.filter(d=>d['rel all']!=''), d=>d['rel all']));
+    var max = d3.max(data, pays => d3.max(pays.data, d=>d['rel all']));
+    y.domain([min, max]);
+}
+
+
+/**
+ * 
+ * @param color       Scale à definir
+ * @param countries    Données à afficher
+ *                  
+ */
+var domainColor = function(color, countries){
+    var continents = new Set(Object.keys(countries).map(c=>countries[c].continent));
+    color.domain(continents);
 }
--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,28 +1,73 @@
-/**
- * Fichier principal permettant de gérer la carte. Ce fichier utilise les autres fichiers
- * que vous devez compléter.
- *
- * /!\ Aucune modification n'est nécessaire dans ce fichier!
- */
 (function (d3, searchBar) {
   "use strict";
 
-
+  var mainPanel = d3.select('#panel')[0][0];
+  
+  
+  // Main graph
+  var width = mainPanel.offsetWidth - 20, height = mainPanel.offsetHeight - 20;
+  var mainSVG = d3.select('#panel svg');
+  var g = mainSVG.append('g');
+  
+  var y = d3.scale.linear().range([height, 0]);
+  var x = d3.scale.linear().range([0, width]);
+  var color = d3.scale.category10();
+  
+  var xAxis = d3.svg.axis().scale(x).orient('bottom');
+  var yAxis = d3.svg.axis().scale(y).orient('left');
+  
+  
+  
   /***** Chargement des données *****/
   d3.queue()
-    .defer(d3.json, "./data/incident.csv")
-    .defer(d3.json, "./data/population.csv")
-    .defer(d3.json, "./data/countryInfos.csv")
+    .defer(d3.csv, "./data/incident.csv")
+    .defer(d3.csv, "./data/population.csv")
+    .defer(d3.csv, "./data/countryInfos.csv")
     .awaitAll(function (error, results) {
-      if (error || results.length !== 1) {
+      if (error || results.length !== 3) {
         throw error;
       }
       var roadIncident = results[0];
       var population = results[1];
       var countryInfos = results[2];
 
-      /***** Prétraitement des données *****/
-      var data = preproc(roadIncident, population);
+    /***** Prétraitement des données *****/
+      var countries = preproccessCountries(countryInfos);
+      var data = preproccess(roadIncident, population, countries);
+      domainX(x, data);
+      domainY(y, data);
+      console.log(countries);
+      domainColor(color, countries);
+      
+      
+    /***** Création du Graphe *****/
+      var gXAxis = g.append('g').classed('axis', true);
+      gXAxis.attr('transform', 'translate(0, %h)'.replace('%h', height) )
+           .call(xAxis)
+              
+      var gYAxis = g.append('g').attr('class', 'axis');
+      gYAxis.call(yAxis)
+         
+      var gLines = g.append('g')
+                  .attr('id', 'lines')
+                  
+      var line = d3.svg.line()
+                   .x(d => x(d.annee))
+                   .y(d => y(d['rel all']))
+                   .interpolate("basis");
+             
+        gLines.selectAll('path')
+            .data(data).enter()
+            .append('path')
+            .attr('d', d => line(d.data))
+            .attr('fill', 'none')
+            .attr('stroke', d => color(countries[d.pays].continent))
+            .attr("stroke-width", 1)
+            .attr('opacity', d=>d.pays=='France'?1:0.3)
+            .classed('tooltip', true)
+            .append('span')
+                .classed('tooltiptext', true)
+                .html(d=>d.pays);
       
       });
 
